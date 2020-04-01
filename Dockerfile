0f73ca41a736740ed21e0664b666579979d62108
Stéphane Colalongo <scolalongo@gmail.com>
2020-04-01 16:32:07 +0200

    Set entrypoint to generate profiles.

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,5 @@ RUN ./aws/install
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 
-CMD [ "/entrypoint.sh" ]
+ENTRYPOINT [ "/entrypoint.sh" ]
+
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 if [ ! -z ${AWS_PROFILES_CREDENTIALS} ]; then
-	echo "PROFILES ACTIVATED"
+	profiles=`echo ${AWS_PROFILES_CREDENTIALS} | tr "#" "\n"`
+	for profile in ${profiles}
+	do
+		profile_data=($(echo ${profile} | tr ";" "\n"))
+		profile_name=${profile_data[0]}
+		profile_access_key=${profile_data[1]}
+		profile_secret_access_key=${profile_data[2]}
+		profile_region=${profile_data[3]}
+
+		echo "Setting profile: ${profile_name}"
+
+		aws configure set aws_access_key_id ${profile_access_key}  --profile ${profile_name}
+		aws configure set aws_secret_access_key ${profile_secret_access_key}  --profile ${profile_name}
+		aws configure set default.region ${profile_region}  --profile ${profile_name}
+	done
 fi
 
 command_line_worker
